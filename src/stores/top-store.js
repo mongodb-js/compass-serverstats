3dc555592ec25b2475ce029288d701178cff918f
Durran Jordan <durran@gmail.com>
2017-04-03 16:10:37 +0200

    Remove unused unit test

--- src/stores/top-store.js
+++ src/stores/top-store.js
@@ -76,6 +76,8 @@ const TopStore = Reflux.createStore({
 
   // Calculate list as current hottest collection (like Cloud and system top)
   top_delta: function(error, response) {
+    console.log(error);
+    console.log(response);
     // stop top command especially if in sharded cluster
     if (error || this.disableTop) {
       return;
